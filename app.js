f149a4615f27802eb36ed79f2dfe5c5547ae142b
ansebanse <146828882+ansebanse@users.noreply.github.com>
2023-10-05 11:30:12 +0200

    Opagver 1&2

--- app.js
+++ app.js
@@ -1,7 +1,96 @@
-//Skriv alt jeres kode her
+<><h1>Programerings Mini opgaver</h1><h2>"Konsol Opgaver"</h2></>
 
-console.log(
-  "Opgave 1.1 - Lav et program, som skriver “Hello World” i konsollen"
-);
-//Lav jeres svar her
-console.log("XX");
+console.log("Opgave 1.1 – Lav et program, som skriver “Hello World” i konsollen");
+console.log("Hello World");
+
+console.log("Opgave 1.2 – Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen");
+console.log("Hello");
+console.log("World")
+
+console.log("Opgave 1.3");
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("**");
+console.log("*");
+
+console.log("Opgave 1.4 - Lav et program, hvor i definere en konstant eller variabel og programmet skriver hvilken type det er, i konsollen. (Altså om det er en int, string, bool osv.")
+
+const goldenratio = 1.618
+console.log(goldenratio)
+
+console.log("Opgave 1.5");
+console.log(10-5)
+console.log(25/3)
+console.log(25%3)
+console.log(25%2)
+console.log(3**2)
+
+console.log("Opgave 1.6");
+let text1="Hello"
+let text2="World"
+
+console.log(text1+text2)
+
+console.log("Opgave 1.7");
+let gf20oline = "GF2 Online"
+console.log(gf20oline,length)
+
+console.log("Opgave 1.8");
+let tilfældigTal = (Math,random()*20)
+
+console.log("Opgave 1.9");
+let number = 23;
+console.log("Dette Program udskriver alle intergallerne altså tal uden decimaler, den spytter derfor number")
+
+console.log(number = "23")
+console.log("Dette Program udskriver ")
+
+console.log(number == "23")
+console.log("Dette Program udskriver ")
+
+console.log(number -= 23)
+console.log("Dette Program udskriver ")
+
+console.log(number += 23)
+console.log("Dette Program udskriver ")
+
+console.log(number += "23")
+
+console.log("Dette Program udskriver ")
+
+console.log(number -= "46")
+console.log("Dette Program udskriver ")
+
+console.log(number != "23")
+console.log("Dette Program udskriver ")
+
+console.log(number **= 3)
+console.log("Dette Program udskriver ")
+
+console.log("Conditionals - If / else statements Opgaver")
+console.log("Opgave 2.1 - Skal skrive et stykke kode som fortæller om et tal (int) er lige eller ulige.")
+
+console.log("Opgave 2.2 - I skal skrive et stykke kode som fortæller om 5 går op i det tal som i vælger (int). Hvis den går op i det skal i skrive: 5 går op i (det tal i har valgt")
+
+console.log("Opgave 3.1 – Lav et program som skriver alle tal fra 1 til 100 ud i konsollen en efter en.")
+var points = new Array(100);
+for (var i=0;i<100;i++) {points[i]+1;}
+
+for (var i=0; i < points.length; i++) {console.log(points[i])}
+
+console.log("Opgave 3.1 – Lav et program som skriver alle tal fra 1) til 100 ud i konsollen en efter en.")
+let i=0
+for (let i = 0; i < 101; i++) {
+  console.log(i);}
+
+console.log(Opgave 3.2–)
+
+
+
+var points = new Array(100);
+for (var i=0;i<100;i++) {points[i]+1;}
+
+for (var i=0; i < points.length; i++) {console.log(points[i])}
+
+console.log("Opgave 3.3" – )
