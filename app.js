fe97873c51b7c1cc02b91cebc0ff75092c77cc22
ansebanse <146828882+ansebanse@users.noreply.github.com>
2023-10-06 10:19:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ console.log("Opgave 1.4 - Lav et program, hvor i definere en konstant eller vari
 
 const goldenratio = 1.618
 console.log(goldenratio)
+console.log(typeof goldenratio)
+
+let a = 1
+console.log(a)
+console.log(typeof a)
+
+console.log("Begge af de konstanter jeg har lavet udskriver Number altså har vi snak om intergers.")
 
 console.log("Opgave 1.5");
 console.log(10-5)
@@ -71,6 +78,7 @@ console.log("Dette Program udskriver ")
 console.log("Conditionals - If / else statements Opgaver")
 console.log("Opgave 2.1 - Skal skrive et stykke kode som fortæller om et tal (int) er lige eller ulige.")
 
+
 console.log("Opgave 2.2 - I skal skrive et stykke kode som fortæller om 5 går op i det tal som i vælger (int). Hvis den går op i det skal i skrive: 5 går op i (det tal i har valgt")
 
 console.log("Opgave 3.1 – Lav et program som skriver alle tal fra 1 til 100 ud i konsollen en efter en.")
@@ -84,8 +92,10 @@ let i=0
 for (let i = 0; i < 101; i++) {
   console.log(i);}
 
-console.log(Opgave 3.2–)
-
+console.log("Opgave 3.2–")
+for (let i=1; i<= 20; i++) {
+   console.log(i+ ""+(i+1)+""+(i+2));
+}
 
 
 var points = new Array(100);
@@ -93,4 +103,11 @@ for (var i=0;i<100;i++) {points[i]+1;}
 
 for (var i=0; i < points.length; i++) {console.log(points[i])}
 
+
 console.log("Opgave 3.3" – )
+console.log(1);
+while (1<100){
+  if (i % 3==0){console.log("Fizz"){
+    else if (i % 5==0){console.log(”Buzz)
+  }
+}
